6ee049bf805f59c6d5ba443d3eada3d05f6bd79d
markjuno <mark@the-juno.com>
2022-04-07 14:43:32 +0300

    add tests

--- src/task2/ecommerce.test.js
+++ src/task2/ecommerce.test.js
@@ -1,5 +1,5 @@
 import { fetchOrderById } from "../api";
-import {fetchAllOrders} from "./ecommerce";
+import {bucketOrdersByUsers2, bucketOrdersByUsers3, fetchAllOrders} from "./ecommerce";
 
 const ORDER_ID = "70ef599e5eca171b2bce84d1"
 test("Ecommerce - fetchOrderById", async () => {
@@ -8,5 +8,15 @@ test("Ecommerce - fetchOrderById", async () => {
 });
 test("Ecommerce - fetchOrderById", async () => {
     let orders = await fetchAllOrders();
+    expect(orders).toBeUndefined();
+});
+
+test("Ecommerce - bucketOrdersByUsers2", async () => {
+    let orders = bucketOrdersByUsers2();
+    expect(orders).toBeUndefined();
+});
+
+test("Ecommerce - bucketOrdersByUsers3", async () => {
+    let orders = await bucketOrdersByUsers3();
     expect(orders).toBeUndefined();
 });
