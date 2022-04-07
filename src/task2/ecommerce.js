26752e35f501cfe2ff7b18a5ef6d1efaede2b02b
markjuno <mark@the-juno.com>
2022-04-07 12:15:49 +0300

    ecommerce - code coverage test

--- src/task2/ecommerce.js
+++ src/task2/ecommerce.js
@@ -9,6 +9,13 @@ export const fetchAllOrders = () => {
     //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
 };
 
+const bucketOrdersByUsers2 = () => {
+    let ordersByUsers = {};
+    //   2. TODO: using the function from section 1 you should now bucket the orders by user.
+    // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
+    return ordersByUsers;
+};
+
 const bucketOrdersByUsers = () => {
     let ordersByUsers = {};
     //   2. TODO: using the function from section 1 you should now bucket the orders by user.
